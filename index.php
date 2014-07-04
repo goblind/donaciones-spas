2ebc4edc3f558cc94de735bb233eefefb11f0a5b
Goblind <admin@goblind.local>
2014-07-03 22:18:11 -0300

    first commit

--- index.php
+++ index.php
@@ -5,20 +5,98 @@
 	<title>Donar</title>
 </head>
 <body>
-	Hello World!!!
-	<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
-		<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
-		<input type='hidden' name='TipoMoneda' value='1'>
-		<input type='hidden' name='PrecioItem' value='50.00'>
-		<input type='hidden' name='E_Comercio' value='3962148'>
-		<input type='hidden' name='NroItem' value='-'>
-		<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
-		<input type='hidden' name='DireccionExito' value='http://'>
-		<input type='hidden' name='DireccionFracaso' value='http://'>
-		<input type='hidden' name='DireccionEnvio' value='0'>
-		<input type='hidden' name='Mensaje' value='1'>
-		<input type='hidden' name='MediosPago' value='13'>
-		<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar-medios_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
-	</form>
+	<div>
+		<h3>Ayudá a nuestros perritos con tu donación.</h3>
+	</div>
+	<div>	
+		<h4>$50</h4>
+		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
+			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
+			<input type='hidden' name='TipoMoneda' value='1'>
+			<input type='hidden' name='PrecioItem' value='50.00'>
+			<input type='hidden' name='E_Comercio' value='3962148'>
+			<input type='hidden' name='NroItem' value='-'>
+			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
+			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
+			<input type='hidden' name='DireccionFracaso' value='http://'>
+			<input type='hidden' name='DireccionEnvio' value='0'>
+			<input type='hidden' name='Mensaje' value='1'>
+			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
+			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
+		</form>
+	</div>	
+	<br>
+	<div>	
+		<h4>$100</h4>
+		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
+			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
+			<input type='hidden' name='TipoMoneda' value='1'>
+			<input type='hidden' name='PrecioItem' value='100.00'>
+			<input type='hidden' name='E_Comercio' value='3962148'>
+			<input type='hidden' name='NroItem' value='-'>
+			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
+			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
+			<input type='hidden' name='DireccionFracaso' value='http://'>
+			<input type='hidden' name='DireccionEnvio' value='0'>
+			<input type='hidden' name='Mensaje' value='1'>
+			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
+			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
+		</form>
+	</div>
+	<br>
+	<div>	
+		<h4>$200</h4>
+		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
+			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
+			<input type='hidden' name='TipoMoneda' value='1'>
+			<input type='hidden' name='PrecioItem' value='200.00'>
+			<input type='hidden' name='E_Comercio' value='3962148'>
+			<input type='hidden' name='NroItem' value='-'>
+			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
+			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
+			<input type='hidden' name='DireccionFracaso' value='http://'>
+			<input type='hidden' name='DireccionEnvio' value='0'>
+			<input type='hidden' name='Mensaje' value='1'>
+			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
+			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
+		</form>
+	</div>	
+	<br>
+	<div>	
+		<h4>$500</h4>
+		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
+			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
+			<input type='hidden' name='TipoMoneda' value='1'>
+			<input type='hidden' name='PrecioItem' value='500.00'>
+			<input type='hidden' name='E_Comercio' value='3962148'>
+			<input type='hidden' name='NroItem' value='-'>
+			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
+			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
+			<input type='hidden' name='DireccionFracaso' value='http://'>
+			<input type='hidden' name='DireccionEnvio' value='0'>
+			<input type='hidden' name='Mensaje' value='1'>
+			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
+			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
+		</form>
+	</div>	
+	<br>
+	<div>	
+		<h4>$1000</h4>
+		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
+			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
+			<input type='hidden' name='TipoMoneda' value='1'>
+			<input type='hidden' name='PrecioItem' value='1000.00'>
+			<input type='hidden' name='E_Comercio' value='3962148'>
+			<input type='hidden' name='NroItem' value='-'>
+			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
+			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
+			<input type='hidden' name='DireccionFracaso' value='http://'>
+			<input type='hidden' name='DireccionEnvio' value='0'>
+			<input type='hidden' name='Mensaje' value='1'>
+			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
+			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
+		</form>
+	</div>	
+
 </body>
 </html>
