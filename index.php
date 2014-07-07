5f6c5ed65a9db7452480ea98697ac3c39fcdeb85
Goblind <admin@goblind.local>
2014-07-07 17:21:21 -0300

    first commi22t

--- index.php
+++ index.php
@@ -14,24 +14,6 @@
 		<img src="spaslogo.jpg"  alt="">
 		<h2>Ayudá a nuestros perritos con tu donación.</h3>
 	</div>
-		<div>	
-		<h4>$25</h4>
-		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
-			<input type='hidden' name='NombreItem' value='Donaciones Perritos Sarmiento'>
-			<input type='hidden' name='TipoMoneda' value='1'>
-			<input type='hidden' name='PrecioItem' value='25.00'>
-			<input type='hidden' name='E_Comercio' value='1170872'>
-			<input type='hidden' name='NroItem' value='-'>
-			<input type='hidden' name='image_url' value='http://donaciones-protectorasarmiento.herokuapp.com/spaslogo.jpg'>
-			<input type='hidden' name='DireccionExito' value='http://donaciones-protectorasarmiento.herokuapp.com/ok.html'>
-			<input type='hidden' name='DireccionFracaso' value='http://'>
-			<input type='hidden' name='DireccionEnvio' value='0'>
-			<input type='hidden' name='Mensaje' value='1'>
-			<input type='hidden' name='MediosPago' value='4,5,6,14,15,13'>
-			<input type='image' src='https://argentina.dineromail.com/imagenes/botones/donar_c.gif?dmbypayu' border='0' name='submit' alt='Pagar con DineroMail'>
-		</form>
-	</div>	
-	<br>
 	<div>	
 		<h4>$50</h4>
 		<form action='https://argentina.dineromail.com/Shop/Shop_Ingreso.asp' method='post'>
